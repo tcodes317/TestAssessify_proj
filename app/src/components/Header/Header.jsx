e4bda2698ef652ed4538f04f81e008b1be1b2155
tcodes317 <tcodes317@gmail.com>
2024-06-12 20:18:20 -0700

    6/12/2024

--- app/src/components/Header/Header.jsx
+++ app/src/components/Header/Header.jsx
@@ -5,18 +5,18 @@ export default function Header(){
     return(
         <>
             <header className="desk:absolute desk:top-0 desk:left-0 desk:w-full desk:z-10">
-                <div className="desk:flex desk:items-center desk:px-2 desk:justify-between desk:bg-white desk:p-2 desk:shadow-sm">
-                    <div className="desk:w-52">
+                <div className="desk:flex gal:flex gal:justify-between desk:items-center desk:px-2 desk:justify-between desk:bg-white gal:py-3 desk:p-2 desk:shadow-sm">
+                    <div className="desk:w-52 gal:w-6/12">
                         <img src={logo} srcset={logo} className="w-52" />
                     </div>
-                    <i className="fa fa-bars desk:hidden px-4 text-2xl"></i>
-                    <div className="gal:absolute desk:static gal:hdden gal:bg-white gal:top-14 gal:w-full gal:left-0">
-                        <nav className="space-x-6 gal:space-x-0 font-normal gal:flex gal:flex-col">
-                            <Link to="/">Home</Link>
-                            <Link to="/about">About Us</Link>
-                            <Link to="/findjob">Find Jobs</Link>
-                            <Link to="/forcompanies">For Companies</Link>
-                            <button className="p-4 py-1.5 text-white px-7 rounded-md bg-lime-500">
+                    <i className="fa fa-bars desk:hidden gal:block px-4 text-2xl"></i>
+                    <div className="gal:absolute gal:z-50 desk:static gal:hdden gal:bg-white gal:top-14 gal:w-full gal:left-0">
+                        <nav className="desk:space-x-6 gal:px-2 desk:justify-end gal:space-x-0 font-normal desk:items-center gal:flex gal:flex-col desk:flex-row">
+                            <Link to="/" className="gal:p-3 gal:hover:bg-lime-500 desk:p-0">Home</Link>
+                            <Link to="/about" className="gal:p-3 gal:hover:bg-lime-500">About Us</Link>
+                            <Link to="/findjob" className="gal:p-3 gal:hover:bg-lime-500">Find Jobs</Link>
+                            <Link to="/forcompanies" className="gal:p-3 gal:hover:bg-lime-500">For Companies</Link>
+                            <button className="p-4 py-1.5 gal:my-3 text-white px-7 rounded-md bg-lime-500">
                                 <Link to="/login">Login</Link>
                             </button>
                         </nav>
